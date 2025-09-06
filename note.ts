bbbe6b7e8925ac165b7698bf2129804d4256e1a3
ahmadmusa <ahmadmusa9805gmail.com>
2025-09-06 17:28:40 +0600

    add some code

--- note.ts
+++ note.ts
@@ -6,7 +6,6 @@
 //                         (for courier, user, admin)
 // item                    (for courier, user, admin)
 // time slot                (for courier, user, admin)
-<<<<<<< HEAD
 // blog                     (home page for user)
 // testimonial
 
@@ -23,22 +22,6 @@
 // payment
 // payout
 // reviews                  
-=======
-// reviews                  
-// blog                     (home page for user)
-// customer support         (for courier, user, admin)
-// testimonial
-// Term and condition
-// Privacy policy
-// analitics
-
-// notification             (for courier, user, admin)
-// jobs                     (for courier, user, admin) (need to discuss for courier part)
-// daily route              (for courier)  (need to discuss for courier part)
-// chat                     (for courier, user, admin)
-// payout
-// payment
->>>>>>> 5de5f4acc7a546bfa99809be1022b68431d56132
 
 // ==================================================================================
 
